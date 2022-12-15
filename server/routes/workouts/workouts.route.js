e78cd273b5b688f22d4d33f422819200e584a0b3
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-12-15 21:47:59 +0500

    create workout route endpoints

---
+++ server/routes/workouts/workouts.route.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const router = express.Router();
+
+// GET all workouts
+router.get("/", (req, res) => {
+  res.json({ msg: "GET all workouts" });
+});
+
+// GET a single workout
+router.get("/:id", (req, res) => {
+  const workoutId = req.params.id;
+  res.json({ msg: `GET ${workoutId} workout` });
+});
+
+// POST a new workout
+router.post("/", (req, res) => {
+  const body = req.body;
+  res.json({ msg: "POST a new workouts", body });
+});
+
+// DELETE a workout
+router.delete("/:id", (req, res) => {
+  const workoutId = req.params.id;
+  res.json({ msg: `DELETE ${workoutId} workout` });
+});
+
+// PATCH a workout
+router.patch("/:id", (req, res) => {
+  const workoutId = req.params.id;
+  res.json({ msg: `UPDATE ${workoutId} workout` });
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,19 @@
-require("dotenv").config();
 const express = require("express");
+const workoutsRouter = require("./routes/workouts/workouts.route");
+require("dotenv").config();
 
 const app = express();
 
 const PORT = process.env.PORT;
+
+app.use(express.json());
 
 app.use((req, res, next) => {
   console.log(req.path, req.method);
   next();
 });
 
-app.get("/", (req, res) => {
-  res.json({ msg: "Welcome to the app!" });
-});
+app.use("/api/workouts", workoutsRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is listening at ${PORT}`);
