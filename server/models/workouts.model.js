28fb7f12a6442390e88977dcce8dfe8c435ad8ad
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-12-16 18:21:50 +0500

    create workouts schema & model using mongoose

---
+++ server/models/workouts.model.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const workoutsSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    reps: {
+      type: Number,
+      required: true,
+    },
+    load: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Workout", workoutsSchema);
