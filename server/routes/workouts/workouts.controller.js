6de8624e10617f77045feb78463ef9192071a6d7
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-12-16 22:05:22 +0500

    abstract out controllers from routes

---
+++ server/routes/workouts/workouts.controller.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const workouts = require("../../models/workouts.model");
+
+// get all workouts
+async function httpGetWorkouts(req, res) {
+  const allWorkouts = await workouts.find({}).sort({ createdAt: -1 });
+
+  res.status(200).json(allWorkouts);
+}
+
+// get a single workout
+async function httpGetWorkout(req, res) {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(404).json({ error: "No such workout exist" });
+  }
+
+  const singleWorkout = await workouts.findById(id);
+
+  if (!singleWorkout) {
+    return res.status(404).json({ error: "No such workout exist" });
+  }
+
+  res.status(200).json(singleWorkout);
+}
+
+// create a new workout
+async function httpCreateWorkout(req, res) {
+  const { title, load, reps } = req.body;
+  try {
+    const workoutDoc = await workouts.create({ title, load, reps });
+    res.status(200).json(workoutDoc);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+}
+
+// delete a workout
+
+// update a workout
+
+module.exports = {
+  httpCreateWorkout,
+  httpGetWorkout,
+  httpGetWorkouts,
+};
--- server/routes/workouts/workouts.route.js
+++ server/routes/workouts/workouts.route.js
@@ -1,31 +1,21 @@
 const express = require("express");
-const workouts = require("../../models/workouts.model");
+
+const {
+  httpCreateWorkout,
+  httpGetWorkout,
+  httpGetWorkouts,
+} = require("./workouts.controller");
 
 const router = express.Router();
 
-// DZjsouZsgVhwb0hS
-
 // GET all workouts
-router.get("/", (req, res) => {
-  res.json({ msg: "GET all workouts" });
-});
+router.get("/", httpGetWorkouts);
 
 // GET a single workout
-router.get("/:id", (req, res) => {
-  const workoutId = req.params.id;
-  res.json({ msg: `GET ${workoutId} workout` });
-});
+router.get("/:id", httpGetWorkout);
 
 // POST a new workout
-router.post("/", async (req, res) => {
-  const { title, load, reps } = req.body;
-  try {
-    const workoutDoc = await workouts.create({ title, load, reps });
-    res.status(200).json(workoutDoc);
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
-});
+router.post("/", httpCreateWorkout);
 
 // DELETE a workout
 router.delete("/:id", (req, res) => {
@@ -40,3 +30,4 @@ router.patch("/:id", (req, res) => {
 });
 
 module.exports = router;
+// DZjsouZsgVhwb0hS
