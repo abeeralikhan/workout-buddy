7fcc42209af2497f1845c9fe4b9afd6e3532b1ef
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-12-18 21:55:21 +0500

    remove log requests middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const workoutsRouter = require("./routes/workouts/workouts.route");
-const logRequests = require("./middlewares/logRequests");
 
 const app = express();
 
 app.use(express.json());
-app.use(logRequests);
 
 app.use("/api/workouts", workoutsRouter);
 
--- server/src/middlewares/logRequests.js
+++
@@ -1,6 +0,0 @@
-function logRequests(req, res, next) {
-  console.log(req.path, req.method);
-  next();
-}
-
-module.exports = logRequests;
