6116c174f3bdbfddb63fe3ac0931ce1986ec5f83
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-12-18 21:15:47 +0500

    create a navbar component and do basic styling

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
