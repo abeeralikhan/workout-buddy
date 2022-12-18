f174c0829f4cf4e039c42b6ce60fefae6ca2a2ea
Abeer Ali Khan <aalkhan@students.uit.edu>
2022-12-18 21:57:27 +0500

    setup morgan middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const morgan = require("morgan");
 const workoutsRouter = require("./routes/workouts/workouts.route");
 
 const app = express();
 
+app.use(morgan("combined")); // combined --> popular logs format
 app.use(express.json());
 
 app.use("/api/workouts", workoutsRouter);
