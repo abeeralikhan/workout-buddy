fb6643915718f099e140c015fab860c11a39ec93
Abeer Ali Khan <aalkhan@students.uit.edu>
2023-02-05 16:55:02 +0500

    implement signup logic

--- server/src/models/users/users.model.js
+++ server/src/models/users/users.model.js
@@ -1,13 +1,28 @@
 const users = require("./users.mongo");
 const { genSalt, hash } = require("bcrypt");
+const { isEmail, isStrongPassword } = require("validator");
 
 async function signup(email, password) {
+  // validation
+  if (!email || !password) {
+    throw Error("All fields must be filled");
+  }
+
+  if (!isEmail(email)) {
+    throw Error("Email is invalid");
+  }
+
+  if (!isStrongPassword(password)) {
+    throw Error("Password is weak");
+  }
+
   const isEmailExist = await users.findOne({ email });
 
   if (isEmailExist) {
     throw Error("Email already in use");
   }
 
+  // securing the password
   const salt = await genSalt(10);
   const hashedPassword = await hash(password, salt);
 
